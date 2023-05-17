d1dc149eb84f2748dbad03704b127ed8e4c2cd70
pelumi <polalekan@gmail.com>
2023-05-17 18:11:43 +0100

    01 - task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+0. Basic dictionary
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A class that inherits from BaseCaching and is a caching system
+    without limit
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        # raise NotImplementedError("put must be implemented in your cache class")
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        # raise NotImplementedError("get must be implemented in your cache class")
+        if key is not None:
+            return self.cache_data.get(key)
+
+        return None
