315ee805a33c424cdb0cedc424b0b25cbc17a352
pelumi <polalekan@gmail.com>
2023-05-18 01:40:52 +0100

    01 - task 3 and 4

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+3. LRU Caching
+"""
+from collections import deque
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    A class that inherits from BaseCaching and is a caching system
+    without limit
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_keys = deque()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            cache_data = self.cache_data
+            cache_keys = self.cache_keys
+
+            if len(cache_keys) >= self.MAX_ITEMS and key not in cache_keys:
+                lru = cache_keys[0]
+                print(f"DISCARD: {lru}")
+                cache_data.pop(lru)
+                cache_keys.popleft()
+                # print('keys:', cache_keys)
+
+            cache_data[key] = item
+            if key not in cache_keys:
+                cache_keys.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None:
+            if key in self.cache_keys:
+                self.__replace(key)
+            return self.cache_data.get(key)
+
+        return None
+
+    def __replace(self, key):
+        """
+        Shuffles the keys list to obtain
+        least recently used item in the cache
+        """
+        cache_keys = self.cache_keys
+
+        cache_keys.remove(key)
+        cache_keys.append(key)
